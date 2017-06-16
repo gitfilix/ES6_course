3a7aac6df92bd7e9c60ce84303319752bf4aeaf2
gitfilix <runlevel07@gmail.com>
2017-06-16 15:51:30 +0200

    let and const block scoping

--- app/index.js
+++ app/index.js
@@ -1,7 +1,16 @@
 // ES 6 new concepts
 // const is read-only
-const emails =['frodo@email.com', 'samwise@email.com', 'merry@email.com']
+const emails =['frodo@email.com', 'samwise@email.com', 'merry@email.com'];
 
-emails.push('pipi@email.com')
+emails.push('pipi@email.com');
 
 console.log(emails);
+
+const limit = 200;
+// block scoping limit in a block
+{
+  const limit= 20;
+  console.log('backstage limit: ', limit);
+}
+
+console.log('overall venue limit', limit);
