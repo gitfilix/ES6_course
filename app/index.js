45d1c57d3299fc80a1c8fd22cf45bcb934a412d4
gitfilix <runlevel07@gmail.com>
2017-06-16 17:45:00 +0200

    destructing p1

--- app/index.js
+++ app/index.js
@@ -1,20 +1,5 @@
 // ES 6 new concepts
-// spread operators
-
-let a = [20, 30, 40, 50];
-let b = [11, 12, ...a, 13];
-console.log(b);
-
-
-let namesD = ['Dana', 'Daphne', 'Duffy'];
-let namesA = ['Alice', ...namesD, 'Amoranama', 'Alphamara'];
-
-console.log(namesA);
-
-
-
-function collect(...x){
-  console.log("collect returns: ", x);
-}
-
-collect(1, 2, 3, 4, 5, 6);
+// destructing
+let z = [4, 5, 6, 7];
+let [four, five] = z;
+console.log(four, five);
