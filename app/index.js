2bac84b816541e756f10a8279724ab30810da060
gitfilix <runlevel07@gmail.com>
2017-06-23 17:07:00 +0200

    map and filter method with arrow

--- app/index.js
+++ app/index.js
@@ -8,10 +8,14 @@ let values = [20, 30, 40];
 //   return n*2;
 // }
 
-let doubled = values.map(n => {
-  return n*2;
-});
-
-
+let doubled = values.map(n => n*2);
 
 console.log('doubled values are: ', doubled);
+
+// filter:
+
+let points = [7, 115, 17, 21, 14, 5, 78, 95, 157, 71, 2];
+
+let highScores = points.filter((n) => n > 15);
+
+console.log('highScores: ', highScores);
