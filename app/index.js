1ac181c4105e25a52bdd2b5258d192e47387ede5
gitfilix <runlevel07@gmail.com>
2017-06-23 13:43:35 +0200

    double map function

--- app/index.js
+++ app/index.js
@@ -1,16 +1,17 @@
 // ES 6 new concepts
 // methods and modules
 
-// varible atached to ne
-let cheer = () => {
-  console.log("normal ArrowFunction with a let-variable: Woohohooo");
-}
-// call it
-cheer();
+let values = [20, 30, 40];
+
+// standalone function double
+// let double = (n) => {
+//   return n*2;
+// }
+
+let doubled = values.map(n => {
+  return n*2;
+});
 
 
 
-// anonymous function expression to 
-setTimeout( ()=> {
-  console.log("Woohohooo ArrowFunctions");
-}, 1000);
+console.log('doubled values are: ', doubled);
